85e15c8513a1f3268fd2ac43445c45239c8085c4
masonwinsauer <mason.winsauer@gmail.com>
2017-12-01 15:54:15 -0600

    Adding a getDataForUser function and changed the URL to point to a local instance of the OWAPI server without ratelimiting

--- Overwatch/app.py
+++ Overwatch/app.py
@@ -1,17 +1,22 @@
 from urllib.request import Request, urlopen
+import urllib.parse
 import json
 import csv
+
+def getDataForUser(jsonurl):
+    req = Request(jsonurl, headers={'User-Agent': 'Mozilla/5.0'})
+    with urlopen(req) as url:
+        player_data = json.loads(url.read().decode())
+        #print(player_data)
 
 with open("users.csv", "rt") as f:
     user_list = list(csv.reader(open("users.csv", "rt")))
     for u in user_list:
-        print(u)
+        #user = "Orange-12457"
+        jsonurl = urllib.parse.quote("http://localhost:4444/api/v3/u/" + str(u[0]) + "/blob", safe=':/')
+        print(jsonurl)
+        getDataForUser(jsonurl)
+        #try:
+        #except (urllib.error.URLError, urllib.error.HTTPError):
+        #    print("Uh Ohhh")
 
-user = "Orange-12457"
-jsonurl = "https://owapi.net/api/v3/u/" + user + "/blob"
-
-#def getDataForUser():
-req = Request(jsonurl, headers={'User-Agent': 'Mozilla/5.0'})
-with urlopen(req) as url:
-    player_data = json.loads(url.read().decode())
-    #print(player_data)
