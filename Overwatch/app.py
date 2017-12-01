fc5a0ca72a191ed1763605a119217973636fde83
masonwinsauer <mason.winsauer@gmail.com>
2017-12-01 00:25:34 -0600

    Added functionality to read from csv

--- Overwatch/app.py
+++ Overwatch/app.py
@@ -1,10 +1,17 @@
 from urllib.request import Request, urlopen
 import json
+import csv
+
+with open("users.csv", "rt") as f:
+    user_list = list(csv.reader(open("users.csv", "rt")))
+    for u in user_list:
+        print(u)
 
 user = "Orange-12457"
 jsonurl = "https://owapi.net/api/v3/u/" + user + "/blob"
 
+#def getDataForUser():
 req = Request(jsonurl, headers={'User-Agent': 'Mozilla/5.0'})
 with urlopen(req) as url:
     player_data = json.loads(url.read().decode())
-    print(player_data)
+    #print(player_data)
